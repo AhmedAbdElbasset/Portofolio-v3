085ce1628d96664ad3d3b41853e749cd649695f7
AhmedAbdElbasset <ahmedalmajek393@gmail.com>
2024-04-23 03:50:01 +0200

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,43 @@
-import HomePage from "./Pages/HomePage"
-import AnimatedCursor from "react-animated-cursor"
+import HomePage from "./Pages/HomePage";
+import AnimatedCursor from "react-animated-cursor";
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 export default function App() {
+  const [load, setLoading] = useState(true);
+  window.onload = function () {
+    setTimeout(() => {
+      setLoading(pervState=> !pervState);
+    }, 1500);
+  };
   return (
     <>
-    <AnimatedCursor 
-      innerSize={8}
-      outerSize={20}
-      color='255,255,255'
-      outerAlpha={0.5}
-      innerScale={0.3}
-      outerScale={5}
-      clickables={[
-        'a',
-        'input[type="text"]',
-        'input[type="email"]',
-        'input[type="number"]',
-        'input[type="submit"]',
-        'input[type="image"]',
-        'label[for]',
-        'select',
-        'textarea',
-        'button',
-        '.link'
-      ]} />
-      <HomePage/>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={20}
+        color="255,255,255"
+        outerAlpha={0.5}
+        innerScale={0.3}
+        outerScale={5}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
+          <div className={`loader ${load?'' :'active'}`}>
+            <div className={`dot-one`}></div>
+            <div className={`dot-two`}>- -</div>
+            <div className={`dot-three`}></div>
+          </div>
+          <HomePage/>
     </>
-  )
+  );
 }
